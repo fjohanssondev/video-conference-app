24ae10f1b59cd328790ea0c36435bdda26989df1
fjohanssonnn <fjohansson.creative@gmail.com>
2025-11-08 12:32:48 +0100

    feat: added theme-provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,24 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import "./index.css";
 
-import { routeTree } from './routeTree.gen'
+import { routeTree } from "./routeTree.gen";
+import { ThemeProvider } from "@/components/theme-provider";
 
-const router = createRouter({ routeTree })
+const router = createRouter({ routeTree });
 
-declare module '@tanstack/react-router' {
+declare module "@tanstack/react-router" {
   interface Register {
-    router: typeof router
+    router: typeof router;
   }
 }
 
-const rootElement = document.getElementById('root')!
+const rootElement = document.getElementById("root")!;
 if (!rootElement.innerHTML) {
-  const root = createRoot(rootElement)
+  const root = createRoot(rootElement);
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
-    </StrictMode>,
-  )
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </StrictMode>
+  );
 }
